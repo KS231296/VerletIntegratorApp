f44ce6067265aa5be750592e49c2d6483a3c9851
KS231296 <44607465+KS231296@users.noreply.github.com>
2018-12-18 00:24:10 +0100

    poprawki

--- src/sample/src/VerletIntegrator.java
+++ src/sample/src/VerletIntegrator.java
@@ -21,9 +21,9 @@ public class VerletIntegrator {
         analyzer.update(tStart, x0, v0);
         double a0 = ca.a(x0);
         for (double t = tStart + krok; t <= tStop; t = t + krok) {
-            double x = x0 + krok * (v0 + krok * a0 / 2);
+            double x = x0 + krok * (v0 + ((krok * a0) / 2));
             double a = ca.a(x);
-            double v = v0 + krok * (a0+a) / 2;
+            double v = v0 + (krok * (a0+a) / 2);
 
             analyzer.update(t, x, v);
             x0 = x;
