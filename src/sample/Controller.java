63ec8049eaba429c9a2249188465cbe207b15748
KS231296 <44607465+KS231296@users.noreply.github.com>
2018-12-18 23:59:03 +0100

    added period update view

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -7,21 +7,27 @@ import javafx.scene.chart.XYChart;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
 import sample.src.Analyzer;
 import sample.src.Pendulum;
 import sample.src.VerletIntegrator;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+
 import java.util.ArrayList;
 
+import static java.lang.Math.abs;
+
 public class Controller {
+
     private double krok;
     private double tStart;
     private double tStop;
     private double x0;
     private double v0;
+
+    @FXML
+    private Text txtOkres;
+
 
     @FXML
     private LineChart<Number, Number> xChart;
@@ -31,6 +37,9 @@ public class Controller {
 
     @FXML
     private LineChart<Number, Number> eChart;
+
+    @FXML
+    private LineChart<Number, Number> chartFazowa;
 
     @FXML
     private TextField txtKrok;
@@ -59,7 +68,7 @@ public class Controller {
             tStop = Double.parseDouble(txtStop.getText());
             x0 = Double.parseDouble(txtX0.getText());
             v0 = Double.parseDouble(txtV0.getText());
-        }catch (IllegalArgumentException e) {
+        } catch (IllegalArgumentException e) {
             new Alert(Alert.AlertType.ERROR, "Przynajmniej jedna z podanych wartości nie jest liczbą").showAndWait();
             return;
         }
@@ -71,20 +80,55 @@ public class Controller {
         XYChart.Series<Number, Number> vVal = new XYChart.Series<>();
         XYChart.Series<Number, Number> xVal = new XYChart.Series<>();
         XYChart.Series<Number, Number> energy = new XYChart.Series<>();
+        XYChart.Series<Number, Number> fazowa = new XYChart.Series<>();
 
         xChart.getData().clear();
         vChart.getData().clear();
         eChart.getData().clear();
+        chartFazowa.getData().clear();
+
         ArrayList energyValues = analyzer.energyTest(pendulum);
         for (int i = 0; i < analyzer.gettValues().size(); i++) {
             vVal.getData().add(new XYChart.Data<>((double) analyzer.gettValues().get(i), (double) analyzer.getvValues().get(i)));
             xVal.getData().add(new XYChart.Data<>((double) analyzer.gettValues().get(i), (double) analyzer.getxValues().get(i)));
             energy.getData().add(new XYChart.Data<>((double) analyzer.gettValues().get(i), (double) energyValues.get(i)));
+            fazowa.getData().add(new XYChart.Data<>((double) analyzer.getxValues().get(i), (double) analyzer.getvValues().get(i)));
         }
         vChart.getData().add(vVal);
         xChart.getData().add(xVal);
         eChart.getData().add(energy);
+        chartFazowa.getData().add(fazowa);
+        try {
+            double period = calcPeriod(analyzer.getxValues(), analyzer.getvValues(), analyzer.gettValues());
+            txtOkres.setText(String.format("%.3f", period));
+        } catch (PeriodNotFoundException e) {
+            txtOkres.setText("Nie znaleziono okresu");
+        }
 
+
+    }
+
+    public double findEps(ArrayList list) {
+        double diff = Math.abs((double) list.get(1) - (double) list.get(0));
+        for (int i = 2; i < list.size(); i++) {
+            double dif = Math.abs((double) list.get(i) - (double) list.get(i - 1));
+            if (dif > diff) {
+                diff = dif;
+            }
+        }
+        return diff * 0.5;
+    }
+
+    public double calcPeriod(ArrayList x, ArrayList v, ArrayList t) throws PeriodNotFoundException {
+        double epsX = findEps(x);
+        double epsV = findEps(v);
+        for (int i = 2; i < x.size(); i++) {
+            if ((double) x.get(0) - epsX <= (double) x.get(i) && (double) x.get(i) <= (double) x.get(0) + epsX && (double) v.get(0) - epsV <= (double) v.get(i) && (double) v.get(i) <= (double) v.get(0) + epsV) {
+                return (double) t.get(i);
+            }
+        }
+
+        throw new PeriodNotFoundException("nie udało się znaleźć okresu");
     }
 
 }
---
+++ src/sample/PeriodNotFoundException.java
@@ -0,0 +1,7 @@
+package sample;
+
+public class PeriodNotFoundException extends Exception {
+    public PeriodNotFoundException(String message) {
+        super(message);
+    }
+}
